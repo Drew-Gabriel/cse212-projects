76dceba5985911a20f584a44a3fd2ff1c66e585f
Drew Gabriel <gabjacz@gmail.com>
2026-07-09 20:37:23 -0500

    Complete Week 2 Queues assignment

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,29 +1,30 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
-
 [TestClass]
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Bob", 1);
+        priorityQueue.Enqueue("Sue", 5);
+        priorityQueue.Enqueue("Tim", 3);
+
+        Assert.AreEqual("Sue", priorityQueue.Dequeue());
+        Assert.AreEqual("Tim", priorityQueue.Dequeue());
+        Assert.AreEqual("Bob", priorityQueue.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
 
-    // Add more test cases as needed below.
+        Assert.ThrowsException<InvalidOperationException>(() =>
+        {
+            priorityQueue.Dequeue();
+        });
+    }
 }
