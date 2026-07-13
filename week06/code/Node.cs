8b736a2a079072947950aa5a91ae8c730679a5f1
Drew Gabriel <gabjacz@gmail.com>
2026-07-13 14:13:44 -0500

    Complete Week 6 assignment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -6,40 +6,65 @@ public class Node
 
     public Node(int data)
     {
-        this.Data = data;
+        Data = data;
     }
 
+    // Problem 1
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
-            // Insert to the left
-            if (Left is null)
+            if (Left == null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
         else
         {
-            // Insert to the right
-            if (Right is null)
+            if (Right == null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
     }
 
+    // Problem 2
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+        {
+            if (Left == null)
+                return false;
+
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right == null)
+                return false;
+
+            return Right.Contains(value);
+        }
     }
 
+    // Problem 4
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left != null)
+            leftHeight = Left.GetHeight();
+
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -46,8 +46,22 @@ public static class Trees
     /// <param name="first">the first index in the sortedNumbers to insert</param>
     /// <param name="last">the last index in the sortedNumbers to insert</param>
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
-    private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
-    {
-        // TODO Start Problem 5
+private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
+{
+    // Base case
+    if (first > last)
+        return;
+
+    // Find the middle element
+    int middle = (first + last) / 2;
+
+    // Insert the middle value
+    bst.Insert(sortedNumbers[middle]);
+
+    // Recursively insert the left half
+    InsertMiddle(sortedNumbers, first, middle - 1, bst);
+
+    // Recursively insert the right half
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
     }
 }
